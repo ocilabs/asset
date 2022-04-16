2b19d56461eb8c497f1e1962b9a8cf15a2d36224
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-16 08:10:08 +0200

    options

--- input.tf
+++ input.tf
@@ -1,7 +1,7 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-variable "schema" {
+variable "options" {
   description = "Input from the schema file."
   type = object({
     class    = string,
@@ -9,7 +9,7 @@ variable "schema" {
   })
 }
 
-variable "config" {
+variable "configuration" {
   description = "Retrieve asset configurations."
   type = object({
     tenancy = any,
