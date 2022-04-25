c76edbba482d277c6b10b1e6c759062fe476b188
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-25 08:13:35 +0200

    Update input.tf

--- input.tf
+++ input.tf
@@ -1,6 +1,17 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+variable "account" {
+  description = "Account parameter"
+  type = object({
+    tenancy_id     = string,
+    class          = number,
+    compartment_id = string,
+    home           = string,
+    user_id        = string
+  })
+}
+
 variable "options" {
   description = "Input from the schema file."
   type = object({
@@ -12,8 +23,7 @@ variable "options" {
 variable "configuration" {
   description = "Retrieve asset configurations."
   type = object({
-    tenancy  = any,
-    resident = any
+    service = any
   })
 }
 
